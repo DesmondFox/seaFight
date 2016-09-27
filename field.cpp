c67c8ca5a1f742ff823fe73efdf3aa075d439c56
DesmondFox <survare@hotmail.com>
2016-09-27 23:51:04 +0300

    ПОЧТИ реализовал проверку на "соседство" кораблей

--- field.cpp
+++ field.cpp
@@ -18,7 +18,7 @@ Field::Field(QWidget *parent) : QWidget(parent)
 
     /// temp
     age = 0;
-    FIELD[3][4] = 1;
+    FIELD[5][4] = 1;
     drawFieldCells();
     tmpStatusToPress = false;
     currentPosition = horizontal;
@@ -96,28 +96,22 @@ void Field::mousePressEvent(QMouseEvent *event)
 
 void Field::mouseMoveEvent(QMouseEvent *event)
 {
+    // TODO: Уменьшить нагрузку. Сделать, чтобы проверка была на каждой клетке, а не пикселе
     if ((event->x() > cellSize+zero_x) && (event->y() > cellSize+zero_y))
     {
         if (setCellsModeFlag)
         {
             c_x = (event->x() - cellSize - zero_x) / cellSize + 1;
             c_y = (event->y() - cellSize - zero_y) / cellSize + 1;
-            if ((c_x == currentx) && (c_y == currenty))
-            {
 
-            }
-            else
-            {
-                clean();
-                drawField();
+            clean();
+            drawField();
 
-                Ship s(c_y, c_x, 2, currentPosition);
-                tmpStatusToPress = drawGhostCell(s, tmpStatusToPress ? QColor(0, 150, 0, 100) : QColor(150, 0, 0, 100));
-                qDebug() << "Status: " << tmpStatusToPress;
-            }
+            Ship s(c_y, c_x, 2, currentPosition);
+            tmpStatusToPress = drawGhostCell(s, tmpStatusToPress ? QColor(0, 150, 0, 100) : QColor(150, 0, 0, 100));
+            qDebug() << "Status: " << tmpStatusToPress << " " << c_x << " - " << c_y;
+
         }
-
-
     }
     drawFieldCells();
     update();
@@ -126,6 +120,15 @@ void Field::mouseMoveEvent(QMouseEvent *event)
 
 void Field::drawShip(const Ship &sh, QColor color)
 {
+    // Б Ы Д Л О К О Д
+    // Ы Ы
+    // Д   Д
+    // Л     Л
+    // О       О
+    // К         К
+    // О           О
+    // Д             Д
+
     QPainter p(pm);
     p.setPen(QPen(color));
     p.setBrush(QBrush(color));
@@ -135,9 +138,30 @@ void Field::drawShip(const Ship &sh, QColor color)
     {
         FIELD[sh.shipCell[i].i-1][sh.shipCell[i].j-1] = 1;
     }
-    for (int i = sh.shipCell[0].j-2; i < sh.shipCell[0].j+2; i++)
-        if (i >= 0 && i <= 9)
-            FIELD[sh.shipCell[0].i][i] = 5;
+
+    int tmpi_min = sh.shipCell[0].i-1;
+    int tmpj_min = sh.shipCell[0].j-1;
+    int tmpi_max = sh.shipCell[sh.shipCell.length()-1].i - 1;
+    int tmpj_max = sh.shipCell[sh.shipCell.length()-1].j - 1;
+
+    qDebug() << tmpi_min << tmpi_max << tmpj_min << tmpj_max;
+    for (int i = tmpi_min - 1; i <= tmpi_max+1; i++)
+    {
+        if (tmpj_min - 1 >= 0 && i >= 0)
+            FIELD[i][tmpj_min-1] = 5;
+        if (tmpj_max < 9 && i >= 0 && i < 10)
+            FIELD[i][tmpj_max+1] = 5;
+
+    }
+
+
+
+    if (sh.positionOfShip == horizontal)
+    {
+
+    }
+
+
 
 }
 
